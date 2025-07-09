4914db0f5a8713dbcc4d419f57ab3b729be1ede6
aayush <aayush@gmail.com>
2025-07-09 16:34:33 +0530

    welcome toats added.  cors set to allow all

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import { toast } from 'react-toastify';
+import { jwtDecode } from 'jwt-decode'; 
+
+
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
@@ -6,6 +11,31 @@ import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
+  useEffect(() => {
+    const showWelcome = sessionStorage.getItem("welcomeUser");
+
+    if (showWelcome) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const decoded = jwtDecode(token);
+
+          toast.success(`Welcome, ${decoded.name}! 👋`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            pauseOnHover: true,
+          });
+        } catch (err) {
+          console.error("Invalid token");
+        }
+      }
+
+      sessionStorage.removeItem("welcomeUser"); // Show toast only once
+    }
+  }, []);
+
+
   return (
     <div>
       <Hero />
